e8203c53e36c8c30f787d27002f7b0c5a1805f25
Gennarro@yandex.ru <Gennarro@yandex.ru>
2024-03-22 16:22:38 +0300

    feat:solve 8 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -92,8 +92,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -107,8 +107,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const dayDiff = Math.round(
+    (new Date(dateEnd) - new Date(dateStart)) / (1000 * 60 * 60 * 24)
+  );
+  return dayDiff + 1;
 }
 
 /**
@@ -128,8 +131,11 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const checkDate = new Date(date);
+  return (
+    new Date(period.start) <= checkDate && checkDate <= new Date(period.end)
+  );
 }
 
 /**
@@ -143,8 +149,22 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const options = {
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+    timeZone: 'UTC',
+  };
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(
+    new Date(date)
+  );
+  return formattedDate;
 }
 
 /**
